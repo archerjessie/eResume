1d6adfcdb2cea39b15a737e0c6d63a52dffb11f8
archerjessie <66236559+archerjessie@users.noreply.github.com>
2020-10-09 23:59:48 +1100

    Updates

---
+++ precache-manifest.b97d39df64a4d2cf767e9477b2dcf0d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c212e371b9959eafc2629495e1ac15",
+    "url": "/eResume/index.html"
+  },
+  {
+    "revision": "e974e4c284c438d8e1a0",
+    "url": "/eResume/static/css/main.6bbba285.chunk.css"
+  },
+  {
+    "revision": "96df8d70179ca01425c2",
+    "url": "/eResume/static/js/2.4581ab6d.chunk.js"
+  },
+  {
+    "revision": "cfdb7e15f78cb0bc3b4c9aea456ee6d5",
+    "url": "/eResume/static/js/2.4581ab6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e974e4c284c438d8e1a0",
+    "url": "/eResume/static/js/main.7d797d2d.chunk.js"
+  },
+  {
+    "revision": "ab8a955b57cf6d1e0be4",
+    "url": "/eResume/static/js/runtime-main.79174098.js"
+  },
+  {
+    "revision": "36dd95a5c271fa822d1b44c97b0ac4a7",
+    "url": "/eResume/static/media/main_bg.36dd95a5.png"
+  }
+]);
